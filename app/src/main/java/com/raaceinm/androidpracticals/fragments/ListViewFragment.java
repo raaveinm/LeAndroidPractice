04b531d7c953a0890b238974b5874e77b51df0ab
raaveinm <dulovkir72@gmail.com>
2025-03-17 09:51:57 +0300

    Task#5 checkpoint

--- app/src/main/java/com/raaceinm/androidpracticals/fragments/ListViewFragment.java
+++ app/src/main/java/com/raaceinm/androidpracticals/fragments/ListViewFragment.java
@@ -1,20 +1,19 @@
 package com.raaceinm.androidpracticals.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.raaceinm.androidpracticals.R;
 
-
 public class ListViewFragment extends Fragment {
-
 
     String[] listItems = {
             "Item 1",
@@ -24,8 +23,10 @@ public class ListViewFragment extends Fragment {
             "Item 5"
     };
 
-    public ListViewFragment() {
+    // For accessing via values
+    //String[] name = getResources().getStringArray(R.id.ArrayID);
 
+    public ListViewFragment() {
     }
 
     public static ListViewFragment newInstance(String param1, String param2) {
@@ -42,16 +43,23 @@ public class ListViewFragment extends Fragment {
         if (getArguments() != null) {
             System.out.println("Arguments: " + getArguments());
         }
-
-        ListView customListView = getView().findViewById(R.id.listViewArray);
-        ArrayAdapter<String> customAdapter = new ArrayAdapter<>(getContext(),
-                android.R.layout.simple_list_item_1, listItems);
-        customListView.setAdapter(customAdapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_list_view, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView customListView = view.findViewById(R.id.listViewArray);
+        ArrayAdapter<String> customAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_list_item_1, listItems);
+        customListView.setAdapter(customAdapter);
+
+        //listItems.setOnClickListener((v)  -> {});
     }
 }
